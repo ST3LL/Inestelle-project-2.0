e584fd2910e4d4d8969678cddaf0f194a6221494
tinou99 <tinou99.f@gmail.com>
2018-11-23 10:14:00 +0100

    Update Analyse.py

--- Analyse de donées/Analyse.py
+++ Analyse de donées/Analyse.py
@@ -7,12 +7,26 @@
 import csv
 import numpy as np
 import sys
+
+
+def stop():
+    a=str(input("Souhaitez-vous continuer ? Oui : o, Non : "))
+    if a == "o" :
+        pass
+    else : 
+        sys.exit(0) 
+
+
+
 print("Bonjour! Bienvenue sur notre plateforme interactive !")
-a = str(input("Pour bien commencer, veuillez cliquer sur 'p' si vous connaissez déjà les fonctions existentes, sinon tapez 'o' :"))
-if a == "q" :
-    sys.exit(0) 
-else : 
-    print("blablabla")
+a = str(input("Pour bien commencer, veuillez cliquer sur 'p' si vous connaissez déjà les fonctions existentes, sinon tapez 'c' :"))
+if a == "c" :
+    pass
+elif a == "P" :
+    print ("Voici les fonctions existantes : blabla")
+stop()
+
+
 
 ############################################################################################################################
 #%% Programme permettant de mettre les notes superieures au plafond ########################################################
@@ -55,7 +69,7 @@ def maj(T,o,n):
         if T[i,n] > o[n] : 
             T[i,n]= o[n] 
     return T
-
+    stop()
         
 def maj_tout(T,o):
     n=0
@@ -63,6 +77,7 @@ def maj_tout(T,o):
         T=maj(T,o,n)
         n+=1
     return T
+    stop()
 
 ############################################################################################################################
 #%% fonction permettant de terminer le major de chaque theme ###############################################################
@@ -76,7 +91,7 @@ def major (l,n):
     a_trier = sorted(a_trier)
     a_trier = a_trier[::-1]
     print(a_trier[1])
-
+    stop()
 
 
 ############################################################################################################################
@@ -113,7 +128,17 @@ def nametorang(name):
         return P.index(name)
     else:
         return "Erreur"
+        
 
+
+    
+#pas fini
+def qui(T,P):
+    for i in range (len(T)):
+        ihih = P[i]
+        eheh = T.index(ihih)
+    return eheh
+            
 
  
 def fiche_eleve(name,T,c):
@@ -123,37 +148,48 @@ def fiche_eleve(name,T,c):
         print(name, rang, " : Les notes de cette eleve sont ",T[rang],", sa moyenne est de ",moyenne)
     else:
         print("Erreur")
+    stop()
+  
     
-    
+############################################################################################################################
+#%% Quels élèves ont eu leur semestre ###################################################################################### 
+############################################################################################################################     
     
 def oui_ou_non(name,T):
     if nametorang(name) != "Erreur":
         rang = nametorang(name)
         if T[rang,4] == 1.0:
-            print(name," a bien eu son semestre !")
+            return True
         else :
-            print(name," n'a pas eu son semestre, et doit passer les rattrapages ...")
+            return False
     else:
-        print("Erreur")
-    
-    
-    
-#pas fini
-def qui(P,T):
-    for i in (len(T) and len(P)):
-        T[i] = P(i)
-        print(T)
-            
+        return None
+ 
 
-#pas fini
-def oui_ou_non_tous(T):
-    d = 0
-    for i in range (len(T)):
-        if T[i,4] == 1.0 :
-            d = nametorang(name)
-            
-            print(name, rang)
+       
+def print_oui_ou_non(name,T):
+    if oui_ou_non(name,T) == True :
+        print(name, "a eu son semestre !")
+    elif oui_ou_non(name,T) == False :
+        print(name,"n'a pas eu son semestre, et doit passer les rattrapages ...")
+    else :
+        print("Erreur")
+    stop()
+
+
+
+def oui_ou_non_tous(P,T):
+    r = []
+    for i in range (len(l)):
+        if oui_ou_non(names[i],l):
+            r.append(names[i])
+    return r
+
+
     
+def print_oui_ou_non_tous(P,T):
+    print(oui_ou_non_tous(P,T))
+    stop()
 
 ############################################################################################################################
 #%% Attribution de chaque ligne de notes a un eleve ########################################################################
@@ -168,8 +204,8 @@ def oui_ou_non_tous(T):
 ############################################################################################################################
 def moy_colonne(n):
     moy = np.mean(T,0)[n]
-    print(moy)
-    
+    print("La moyenne de cette UE est de",moy)
+    stop()
     
 ############################################################################################################################   
 #%% Moyenne de chaque eleve ################################################################################################
@@ -178,14 +214,36 @@ def moy_eleve(name,T,c):  #e : numéro de ligne du tableau et c : Coefficient ic
     m=0
     if nametorang(name) != "Erreur":
         rang = nametorang(name)
+        for i in range(len(T[rang])):
+            m += T[rang][i]  #rang i dans la liste e (ligne du tableau) de la liste T
+        m = m/c #On considère qu'une note sur 50 a un coefficient 1 dans notre cas actuel
+        return m
     else:
         print("Erreur")
-    for i in range(len(T[rang])):
-        m += T[rang][i]  #rang i dans la liste e (ligne du tableau) de la liste T
-    m = m/c #On considère qu'une note sur 50 a un coefficient 1 dans notre cas actuel
-    return m
+    stop()
 
 
+############################################################################################################################   
+#%% Courbe nb XP <=> note sur 20 ###########################################################################################
+############################################################################################################################ 
 
+import matplotlib.pyplot as plt
+import numpy as np
 
+AA = [3,8,16,25,36,48,62,78,94,112,131,151,172,194,218,243,267,294,321,350]
+BB = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 
+def courbe_nbXP_note20(plafond):
+    x=np.linspace(0,plafond+30,10)
+    plt.plot(AA,BB)
+    plt.ylabel('Nombre de XP')
+    plt.xlabel("Note sur 20")
+    plt.show()
+
+#pas fini
+def quelle_note?(XP,AA,BB):
+    for i in AA:
+        while AA[i] =< XP =< AA[i+1]:
+            AA[i] = BB[i]
+            AA[i+1] = BB[i+1]
+            nXP = 
